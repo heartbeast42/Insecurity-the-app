97399882a30beb6614cc604eca51c94a1a1cdc75
Clark Walker <303walker@gmail.com>
2018-01-23 11:43:52 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-
 import React, { Component } from 'react';
 import { Router, browserHistory, Route, Link } from 'react-router';
 import logo from './logo.svg';
 import './App.css';
 
 const Page = ({ title }) => (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>{title}</h2>
-      </div>
-      <p className="App-intro">
-        This is the {title} page.
-      </p>
-      <p>
-        <Link to="/">Home</Link>
-      </p>
-      <p>
-        <Link to="/about">About</Link>
-      </p>
-      <p>
-        <Link to="/settings">Settings</Link>
-      </p>
+  <div className="App">
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>{title}</h2>
     </div>
+    <p className="App-intro">
+      This is the {title} page.
+    </p>
+    <p>
+      <Link to="/">Home</Link>
+    </p>
+    <p>
+      <Link to="/about">About</Link>
+    </p>
+    <p>
+      <Link to="/settings">Settings</Link>
+    </p>
+  </div>
 );
 
 const Home = (props) => (
@@ -50,4 +49,4 @@ class App extends Component {
 }
 
 export default App;
-view rawpwa-starter.js hosted with ❤ by GitHub
+// view rawpwa-starter.js hosted with ❤ by GitHub
