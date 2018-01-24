da05fd6df4ee74ce9ebbcbed38c6afd6c21a0531
Clark Walker <303walker@gmail.com>
2018-01-24 16:56:37 -0700

    update

--- src/navbar.js
+++ src/navbar.js
@@ -3,10 +3,14 @@ import React from 'react';
 const Navbar = (props) => {
   return (
     <div className="sidebar bar-block">
-      <img class="close" src="img/close.png"/>
-      <a href="#" className="w3-bar-item w3-button">Link 1</a>
-      <a href="#" className="w3-bar-item w3-button">Link 2</a>
-      <a href="#" className="w3-bar-item w3-button">Link 3</a>
+      <div className="close-div">
+        <img className="close" src="img/close.png"/>
+      </div>
+      <div className="navItems">
+        <a href="#" className="w3-bar-item w3-button">Link 1</a>
+        <a href="#" className="w3-bar-item w3-button">Link 2</a>
+        <a href="#" className="w3-bar-item w3-button">Link 3</a>
+      </div>
     </div>
   )
 }
