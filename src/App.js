205cca354593392477b2b5602c799dd0c6f352a4
Clark Walker <303walker@gmail.com>
2018-01-25 10:59:02 -0700

    update

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@ import { Router, browserHistory, Route, Link } from 'react-router';
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './navbar.js';
-
 import SideNav, { Nav, NavIcon, NavText } from 'react-sidenav';
 import SvgIcon from 'react-icons-kit';
-
 import { ic_aspect_ratio } from 'react-icons-kit/md/ic_aspect_ratio';
 import { ic_business } from 'react-icons-kit/md/ic_business';
 
 
 const Page = ({ title }) => (
   <div className="App">
-    {/* <Navbar/> */}
-    {/* <SideNav selected='xyz'></SideNav> */}
-    <MySideNav/>
+    <Navbar/>
+    {/* <MySideNav/> */}
     <main>
       <div className="App-header">
 
@@ -48,32 +45,17 @@ const Page = ({ title }) => (
   </div>
 );
 
-const MySideNav = () => (
-  <div style={{background: '#2c3e50', color: '#FFF', width: 220}}>
-    <SideNav highlightColor='#E91E63' highlightBgColor='#00bcd4' defaultSelected='sales'>
-      <Nav id='dashboard'>
-        <NavIcon><SvgIcon size={20} icon={ic_aspect_ratio}/></NavIcon>
-        <NavText> Dashboard </NavText>
-      </Nav>
-      <Nav id='sales'>
-        <NavIcon><SvgIcon size={20} icon={ic_business}/></NavIcon>
-        <NavText> Sales </NavText>
-      </Nav>
-    </SideNav>
-  </div>
-)
+const Home = (props) => (
+  <Page title="Home"/>
+);
 
-// const Home = (props) => (
-//   <Page title="Home"/>
-// );
-//
-// const About = (props) => (
-//   <Page title="About"/>
-// );
-//
-// const Settings = (props) => (
-//   <Page title="Settings"/>
-// );
+const About = (props) => (
+  <Page title="About"/>
+);
+
+const Settings = (props) => (
+  <Page title="Settings"/>
+);
 
 class App extends Component {
   render() {
--- src/navbar.js
+++ src/navbar.js
@@ -1,5 +1,16 @@
 import React from 'react';
+
+// import React, { Component } from 'react';
+import { Router, browserHistory, Route, Link } from 'react-router';
+
+import SideNav, { Nav, NavIcon, NavText } from 'react-sidenav';
+import SvgIcon from 'react-icons-kit';
+
+import { ic_aspect_ratio } from 'react-icons-kit/md/ic_aspect_ratio';
+import { ic_business } from 'react-icons-kit/md/ic_business';
 // import nav from './scripts.js'
+
+/* * /
 
 const Navbar = (props) => {
   return (
@@ -15,5 +26,67 @@ const Navbar = (props) => {
     </div>
   )
 }
+
+/*/
+const Navbar = () => (
+  <div id="mySidebar" className="sidebar bar-block">
+    <SideNav highlightColor='#E91E63' highlightBgColor='#00bcd4' defaultSelected='sales'>
+      <div className="close-div">
+        <img className="close" src="img/close.png" />
+      </div>
+
+
+      <div className="navItems">
+        <a href="#" className="w3-bar-item w3-button">Link 1</a>
+        <a href="#" className="w3-bar-item w3-button">Link 2</a>
+        <a href="#" className="w3-bar-item w3-button">Link 3</a>
+      </div>
+
+
+      <Nav id='dashboard'>
+        <NavIcon><SvgIcon size={20} icon={ic_aspect_ratio}/></NavIcon>
+        <NavText> Dashboard </NavText>
+      </Nav>
+      <Nav id='sales'>
+        <NavIcon><SvgIcon size={20} icon={ic_business}/></NavIcon>
+        <NavText> Sales </NavText>
+      </Nav>
+
+
+
+      <p className="App-intro">
+        {/* This is the {title} page. */}
+      </p>
+      <p>
+        <Link to="/">Home</Link>
+      </p>
+      <p>
+        <Link to="/about">About</Link>
+      </p>
+      <p>
+        <Link to="/settings">Settings</Link>
+      </p>
+
+
+
+
+
+
+    </SideNav>
+  </div>
+)
+/* */
+
+// const Home = (props) => (
+//   <Page title="Home"/>
+// );
+//
+// const About = (props) => (
+//   <Page title="About"/>
+// );
+//
+// const Settings = (props) => (
+//   <Page title="Settings"/>
+// );
 
 export default Navbar;
