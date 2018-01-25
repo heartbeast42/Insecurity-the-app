cb12c5ed5f8f334e40a3b3cf594dee0b6b9f7271
Clark Walker <303walker@gmail.com>
2018-01-25 10:19:37 -0700

    update

--- src/App.js
+++ src/App.js
@@ -4,16 +4,28 @@ import { Router, browserHistory, Route, Link } from 'react-router';
 import './App.css';
 import Navbar from './navbar.js';
 
+import SideNav, { Nav, NavIcon, NavText } from 'react-sidenav';
+import SvgIcon from 'react-icons-kit';
+
+import { ic_aspect_ratio } from 'react-icons-kit/md/ic_aspect_ratio';
+import { ic_business } from 'react-icons-kit/md/ic_business';
+
+
 const Page = ({ title }) => (
   <div className="App">
-    <Navbar/>
+    {/* <Navbar/> */}
+    {/* <SideNav selected='xyz'></SideNav> */}
+    <MySideNav/>
     <main>
       <div className="App-header">
+
+
 
 
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1>Insecurity the app</h1>
         {/* <h2>{title}</h2> */}
+
       </div>
 
       {/* <p className="App-intro">
@@ -29,9 +41,27 @@ const Page = ({ title }) => (
         <Link to="/settings">Settings</Link>
       </p> */}
 
+
+
+
     </main>
   </div>
 );
+
+const MySideNav = () => (
+  <div style={{background: '#2c3e50', color: '#FFF', width: 220}}>
+    <SideNav highlightColor='#E91E63' highlightBgColor='#00bcd4' defaultSelected='sales'>
+      <Nav id='dashboard'>
+        <NavIcon><SvgIcon size={20} icon={ic_aspect_ratio}/></NavIcon>
+        <NavText> Dashboard </NavText>
+      </Nav>
+      <Nav id='sales'>
+        <NavIcon><SvgIcon size={20} icon={ic_business}/></NavIcon>
+        <NavText> Sales </NavText>
+      </Nav>
+    </SideNav>
+  </div>
+)
 
 // const Home = (props) => (
 //   <Page title="Home"/>
