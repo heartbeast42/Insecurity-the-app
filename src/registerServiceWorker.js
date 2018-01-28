a3e91d5be6eb6b7377128c4ca347b90dba9f3cf9
Clark Walker <303walker@gmail.com>
2018-01-27 22:54:39 -0700

    update

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,6 +1,7 @@
-if ('serviceWorker' in navigator) {
+export default function registerServiceWorker(){
+  if ('serviceWorker' in navigator) {
 
-  navigator.serviceWorker
+    navigator.serviceWorker
     .register('./service-worker.js', { scope: './' })
     .then(function(registration) {
       console.log("Service Worker Registered");
@@ -9,38 +10,40 @@ if ('serviceWorker' in navigator) {
       console.log("Service Worker Failed to Register", err);
     })
 
+  }
 }
+console.log("THIS is registerServiceWorker.js");
 
 // Function to perform HTTP request
-var get = function(url) {
-  return new Promise(function(resolve, reject) {
+// var get = function(url) {
+//   return new Promise(function(resolve, reject) {
+//
+//     var xhr = new XMLHttpRequest();
+//     xhr.onreadystatechange = function() {
+//         if (xhr.readyState === XMLHttpRequest.DONE) {
+//             if (xhr.status === 200) {
+//                 var result = xhr.responseText
+//                 result = JSON.parse(result);
+//                 resolve(result);
+//             } else {
+//                 reject(xhr);
+//             }
+//         }
+//     };
+//
+//     xhr.open("GET", url, true);
+//     xhr.send();
+//
+//   });
+// };
 
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                var result = xhr.responseText
-                result = JSON.parse(result);
-                resolve(result);
-            } else {
-                reject(xhr);
-            }
-        }
-    };
 
-    xhr.open("GET", url, true);
-    xhr.send();
-
-  });
-};
-
-
-get('https://api.nasa.gov/planetary/earth/imagery?api_key=fWfSMcDzyHfMuH3BW6jiIUBYaj3hKRyKBRTBqgEQ')
-  .then(function(response) {
-    // There is an issue with the image being pulled from the API, so using a different one instead
-    document.getElementsByClassName('targetImage')[0].src = "https://api.nasa.gov/images/earth.png";
-
-  })
-  .catch(function(err) {
-    console.log("Error", err);
-  })
+// get('https://api.nasa.gov/planetary/earth/imagery?api_key=fWfSMcDzyHfMuH3BW6jiIUBYaj3hKRyKBRTBqgEQ')
+//   .then(function(response) {
+//     // There is an issue with the image being pulled from the API, so using a different one instead
+//     document.getElementsByClassName('targetImage')[0].src = "https://api.nasa.gov/images/earth.png";
+//
+//   })
+//   .catch(function(err) {
+//     console.log("Error", err);
+//   })
